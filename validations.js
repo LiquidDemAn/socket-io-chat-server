69639de46d4b0ac6c6a44783ca4165a8eb196d70
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-12-07 15:30:04 +0200

    created validations for user

---
+++ validations.js
@@ -0,0 +1,22 @@
+import { body } from 'express-validator';
+
+export const regsiterValidation = [
+	body('username', 'Username is invalid')
+		.isString()
+		.isLength({ min: 3 })
+		.not()
+		.custom((value) => {
+			const hasSpecialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+			const hasNumber = /\d/;
+
+			return hasSpecialChars.test(value) || hasNumber.test(value);
+		}),
+	,
+	body('email', 'Incorrect email format').isEmail(),
+	body('password', 'Password must be greater than 8 characters')
+		.isString()
+		.isLength({
+			min: 8,
+		}),
+	,
+];
